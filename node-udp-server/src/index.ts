9178da1a9c61f7506e2ba883b9a71571bd519979
Joakim Tysseng <joakim.tysseng@bekk.no>
2022-08-19 16:15:42 +0200

    Minimal UDP server added.

--- node-udp-server/src/index.ts
+++ node-udp-server/src/index.ts
@@ -1,2 +1,31 @@
-const message = 'Hello, world!';
-console.log(message);
+import udp from 'dgram'
+
+const server = udp.createSocket('udp4');
+
+server.on('listening',() => {
+    const address = server.address();
+    const port = address.port;
+    console.log(`Super secret UDP server is listening on port ${port}`);
+});
+
+server.on('message',(msg,info) => {
+    console.log(`Data received from client: ${msg.toString()}`);
+    console.log(`Received ${msg.length} bytes from ${info.address}:${info.port}\n`);
+
+    //sending msg to the client
+    const response = Buffer.from('From server: your msg is received');
+    server.send(response,info.port,'localhost',(error) => {
+        if(error){
+            console.log('Got error', error);
+        } else{
+            console.log('Data sent');
+        }
+    });
+});
+
+server.on('error',(error) => {
+    console.log('Error: ' + error);
+    server.close();
+});
+
+server.bind(80);
