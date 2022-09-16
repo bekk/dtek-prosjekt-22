f5d92c6f3912f41587b424f8e7d86afb433efc50
Trym Sneltvedt <trym.sneltvedt@bekk.no>
2022-09-16 16:51:14 +0200

    Add websockets

--- node-udp-server/src/index.ts
+++ node-udp-server/src/index.ts
@@ -1,97 +1,176 @@
-import udp from 'dgram'
-import http from 'node:http'
+import udp from "node:dgram";
+import http from "node:http";
+import EventEmitter from "node:events";
+import { WebSocketServer } from "ws";
 
 interface Database {
-    store: (msg: string) => void;
-    retrieve: () => Array<DatabaseChunk>;
+  append: (id: string, data: string) => void;
+  retrieve: () => Array<DatabaseChunk>;
+  addAppendListener: (fn: ListenerFn) => void;
+  removeAppendListener: (fn: ListenerFn) => void;
 }
 
 interface DatabaseChunk {
-    msg: string;
-    ts: Date;
+  id: string;
+  data: string;
+  ts: Date;
 }
+
+type ListenerFn = (newChunk: DatabaseChunk) => void;
 
 function createDatabase(): Database {
-    let state: Array<DatabaseChunk> = [];
-    return {
-        store(msg: string) {
-            state.push({msg, ts: new Date()});
-        },
-        retrieve() {
-            return state.slice();
-        }
+  const emitter = new EventEmitter();
+  let state: Array<DatabaseChunk> = [];
+  return {
+    append(id: string, data: string) {
+      const ts = new Date();
+
+      const chunk = { id, data, ts };
+
+      // Store chunk
+      state.push(chunk);
+
+      // Publish chunk event
+      emitter.emit("chunk", chunk);
+    },
+    retrieve() {
+      return state.slice();
+    },
+    addAppendListener(fn: ListenerFn) {
+      emitter.on("chunk", fn);
+    },
+    removeAppendListener(fn: ListenerFn) {
+      emitter.removeListener("chunk", fn);
+    },
+  };
+}
+
+function createWebServer({ port, db }: { port: number; db: Database }) {
+  const wsServer = new WebSocketServer({ port: 8002 });
+
+  db.addAppendListener((data) =>
+    wsServer.clients.forEach((ws) => ws.send(JSON.stringify(data)))
+  );
+
+  const server = http.createServer(async (req, res) => {
+    const host = req.headers.host || "localhost";
+    const url = new URL(`http://${host}${req.url}`);
+
+    if (req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      switch (url.pathname) {
+        case "/metrics":
+          const metricsData = {
+            db: {
+              chunkNum: db.retrieve().length,
+            },
+            ws: {
+              connectionNum: wsServer.clients.size,
+            },
+          };
+          res.write(JSON.stringify(metricsData));
+          break;
+        default:
+          const data = { chunks: db.retrieve() };
+          res.write(JSON.stringify(data));
+      }
+
+      res.end();
     }
+  });
+
+  server.listen(port, () =>
+    console.log(`Super secret HTTP/WebSocket server listening on port ${port}`)
+  );
 }
 
-function createWebServer({port, db}: {port: number, db: Database}) {
-    const server = http.createServer(async (req, res) => {
-        if (req.method === 'GET') {
-            const data = { data: db.retrieve() };
-            res.writeHead(200, {'content-type': 'application/json'});
-            res.write(JSON.stringify(data));
-            res.end();
-        }
-    });
+function createDatagramServer({
+  port,
+  db,
+  demo = false,
+}: {
+  port: number;
+  db: Database;
+  demo?: boolean;
+}) {
+  const server = udp.createSocket("udp4");
 
-    server.listen(port, () => console.log(`Super secret HTTP server listening on port ${port}`));
-}
+  /*
+    Message format
 
-function createDatagramServer({port, db, demo = false}: {port: number, db: Database, demo?: boolean}) {
-    const server = udp.createSocket('udp4');
-    
-    const parseMsg = (msg: string) => {
-        const msgParts = msg.split(';');
-        switch (msgParts[0]) {
-            case 'ypr':
-                console.log(`y: ${msgParts[1]}, p: ${msgParts[2]}, r: ${msgParts[3]}`);
-                return
-            default:
-                console.log(`Unknown msg: ${msg}`)
-                return;
-        }
+    <id>;<data>
+
+    id   = /[A-Za-z0-9]+/
+    data = any data
+
+    */
+
+  // const parseMsg = (msg: string) => {
+  //     const msgParts = msg.split(';');
+  //     switch (msgParts[0]) {
+  //         case 'ypr':
+  //             console.log(`y: ${msgParts[1]}, p: ${msgParts[2]}, r: ${msgParts[3]}`);
+  //             return
+  //         default:
+  //             console.log(`Unknown msg: ${msg}`)
+  //             return;
+  //     }
+  // }
+
+  const MSG_FORMAT_REGEX = new RegExp(/^[a-zA-Z0-9]+;.*$/);
+
+  server.on("listening", () => {
+    const serverAddress = server.address().address;
+    const serverPort = server.address().port;
+    console.log(
+      `Super secret UDP server is listening on ${serverAddress} port ${serverPort}`
+    );
+
+    if (demo) {
+      const client = udp.createSocket("udp4");
+
+      setInterval(
+        () => client.send("demo;ypr;1;2;3", serverPort, serverAddress),
+        1000
+      );
+
+      console.log(`Starting in demo mode.`);
+    }
+  });
+
+  server.on("message", (msg, info) => {
+    const msgString = msg.toString();
+
+    if (!MSG_FORMAT_REGEX.test(msgString)) {
+      console.info(
+        `level=info msg="Invalid message received" received="${msgString}" address=${info.address}`
+      );
+      return;
     }
 
-    server.on('listening', () => {
-        const serverAddress = server.address().address;
-        const serverPort = server.address().port;
-        console.log(`Super secret UDP server is listening on ${serverAddress} port ${serverPort}`);
-    
-        if (demo) {
-            const client = udp.createSocket('udp4');
+    const sepIndex = msgString.indexOf(";");
+    const id = msgString.slice(0, sepIndex);
+    const data = msgString.slice(sepIndex + 1);
 
-            setInterval(() => {
-                // const data = Buffer.from('ypr;1;2;3');
-                client.send('ypr;1;2;3', serverPort, serverAddress);
-            }, 1000);
+    console.debug(
+      `level=debug msg="Message received" id=${id} data="${data}" address=${info.address}`
+    );
+    db.append(id, data);
+  });
 
-            console.log(`Starting in demo mode.`);
+  server.on("error", (error) => {
+    console.log("Error: " + error);
+    server.close();
+  });
 
-        }
-    });
-
-    server.on('message', (msg, info) => {
-        db.store(msg.toString());
-        
-        parseMsg(msg.toString());
-
-    });
-
-    server.on('error', (error) => {
-        console.log('Error: ' + error);
-        server.close();
-    });
-
-    
-
-    server.bind(port);
+  server.bind(port);
 }
 
+console.log("STARTING");
 
-console.log("STARTING")
-
-const isDemo = process.argv[2] === '--demo';
+const isDemo = process.argv[2] === "--demo";
 
 const db = createDatabase();
 
-createDatagramServer({port: 8000, db, demo: isDemo});
-createWebServer({port: 8001, db});
+createDatagramServer({ port: 8000, db, demo: isDemo });
+createWebServer({ port: 8001, db });
