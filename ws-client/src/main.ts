ddd9c43060e0b09b8a1a85e15328d947211eb0b8
Anders Larsen <andelar96@gmail.com>
2022-09-19 10:51:08 +0200

    Mer interessant visualisering hentet fra interveven

--- ws-client/src/main.ts
+++ ws-client/src/main.ts
@@ -1,41 +1,32 @@
 import P5 from "p5";
 
-type Shape = {
-  x: number;
-  y: number;
-  size: number;
-  fill: string;
-};
-
 // Creating the sketch itself
 const sketch = (p: P5) => {
-  const li: Shape[] = [];
+  let shader: P5.Shader;
+
+  p.preload = () => {
+    shader = p.loadShader("src/vert.glsl", "src/frag.glsl");
+  };
 
   p.setup = () => {
-    // Creating and positioning the canvas
-    const canvas = p.createCanvas(1920, 1080);
+    const canvas = p.createCanvas(1920, 1080, p.WEBGL);
     canvas.parent("app");
-
-    // Configuring the canvas
-    p.background("antiquewhite");
-    p.strokeWeight(10);
-
-    for (let i = 0; i < 1000; i++) {
-      li.push({
-        x: p.random(p.width),
-        y: p.random(p.height),
-        size: 100,
-        fill: p.random(["#5C4B51", "#8CBEB2", "#F2EBBF", "#F3B562", "#F06060"]),
-      });
-    }
+    p.noStroke();
+    p.shader(shader);
   };
 
   // The sketch draw method
   p.draw = () => {
-    for (const shape of li) {
-      p.fill(shape.fill);
-      p.circle(shape.x, shape.y, p.noise(shape.x * 0.005, shape.y * 0.005) * shape.size);
-    }
+    p.background("antiquewhite");
+
+    shader.setUniform("uFrameCount", p.frameCount * 2);
+
+    p.rotateX(p.frameCount * 0.02);
+    p.rotateY(p.frameCount * 0.01);
+
+    // Draw some geometry to the screen
+    // We're going to tessellate the sphere a bit so we have some more geometry to work with
+    p.sphere(p.noise(p.frameCount * 0.001) * p.height, 200, 200);
   };
 };
 
